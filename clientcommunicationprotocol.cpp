bc17b73ea48685af6584e5127e636d91dc0a79de
JacoDieWizard <JacoDieWizard@users.noreply.github.com>
2014-09-20 21:08:28 +0200

    first commit

---
+++ clientcommunicationprotocol.cpp
@@ -0,0 +1,5 @@
+#include "clientcommunicationprotocol.h"
+
+ClientCommunicationProtocol::ClientCommunicationProtocol()
+{
+}
---
+++ clientcommunicationprotocol.h
@@ -0,0 +1,10 @@
+#ifndef CLIENTCOMMUNICATIONPROTOCOL_H
+#define CLIENTCOMMUNICATIONPROTOCOL_H
+
+class ClientCommunicationProtocol
+{
+public:
+    ClientCommunicationProtocol();
+};
+
+#endif // CLIENTCOMMUNICATIONPROTOCOL_H
